fdcc69be021f638027ceaeb3af44f12106090405
szymenn <ilikedzimi@gmail.com>
2019-07-16 12:02:37 +0200

    code cleanup

--- ToDoListApi/Models/ToDoBindingModel.cs
+++ ToDoListApi/Models/ToDoBindingModel.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoListApi.Models
 {
     public class ToDoBindingModel
     {
+        [Required]
         public string Task { get; set; }
+        [Required]
         public DateTime Date { get; set; }
     }
 }
--- ToDoListApi/Models/UserBindingModel.cs
+++ ToDoListApi/Models/UserBindingModel.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoListApi.Models
 {
     public class UserBindingModel
     {
+        [Required]
         public string UserName { get; set; }
+        
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
