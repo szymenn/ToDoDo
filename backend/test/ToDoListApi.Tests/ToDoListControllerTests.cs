486225668086e1a8308d7bd06c2268e61c067f23
szymenn <ilikedzimi@gmail.com>
2019-07-24 20:01:06 +0200

    Add ToDoListController tests

--- backend/test/ToDoListApi.Tests/ToDoListControllerTests.cs
+++ backend/test/ToDoListApi.Tests/ToDoListControllerTests.cs
@@ -1,6 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using ToDoListApi.Controllers;
+using ToDoListApi.Entities;
+using ToDoListApi.Models;
 using ToDoListApi.Services;
 using Xunit;
 
@@ -8,6 +15,128 @@ namespace ToDoListApi.Tests
 {
     public class ToDoListControllerTests
     {
-   
-    }
+        [Fact]
+        public void GetToDos_ByDefault_ReturnsOkObjectResult()
+        {
+            var mapperStub = new Mock<IMapper>();
+            var toDoServiceStub = new Mock<IToDoService>();
+            toDoServiceStub.Setup(e => e.GetToDos(It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            }));
+
+            var controller = new ToDoListController
+                (toDoServiceStub.Object, mapperStub.Object)
+                {
+                    ControllerContext = new ControllerContext
+                    {
+                        HttpContext = new DefaultHttpContext
+                        {
+                            User = user
+                        }
+                    }
+                };
+
+            var result = controller.GetToDos();   
+            
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void AddToDo_ByDefault_ReturnsOkObjectResult()
+        {
+            var mapperStub = new Mock<IMapper>();
+            var toDoServiceStub = new Mock<IToDoService>();
+            toDoServiceStub.Setup(e => e.AddToDo(It.IsAny<ToDo>(), It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+            
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            }));
+
+            var controller = new ToDoListController
+                (toDoServiceStub.Object, mapperStub.Object)
+                {
+                    ControllerContext = new ControllerContext
+                    {
+                        HttpContext = new DefaultHttpContext
+                        {
+                            User = user
+                        }
+                    }
+                };
+
+            var result = controller.AddToDo(It.IsAny<ToDoBindingModel>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void DeleteToDo_ByDefault_ReturnsOkObjectResult()
+        {
+            var mapperStub = new Mock<IMapper>();
+            var toDoServiceStub = new Mock<IToDoService>();
+            toDoServiceStub.Setup(e => e.DeleteToDo(It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+            
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            }));
+
+            var controller = new ToDoListController
+                (toDoServiceStub.Object, mapperStub.Object)
+                {
+                    ControllerContext = new ControllerContext
+                    {
+                        HttpContext = new DefaultHttpContext
+                        {
+                            User = user
+                        }
+                    }
+                };
+
+            var result = controller.DeleteToDo(It.IsAny<Guid>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void UpdateToDo_ByDefault_ReturnsOkObjectResult()
+        {
+            var mapperStub = new Mock<IMapper>();
+            var toDoServiceStub = new Mock<IToDoService>();
+            toDoServiceStub.Setup(e => e.UpdateToDo
+                (It.IsAny<ToDo>(),
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>()))
+                .Returns(new ToDo());
+            
+            
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            }));
+
+            var controller = new ToDoListController
+                (toDoServiceStub.Object, mapperStub.Object)
+                {
+                    ControllerContext = new ControllerContext
+                    {
+                        HttpContext = new DefaultHttpContext
+                        {
+                            User = user
+                        }
+                    }
+                };
+
+            var result = controller.UpdateToDo(It.IsAny<ToDoBindingModel>(), It.IsAny<Guid>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }    
 }
