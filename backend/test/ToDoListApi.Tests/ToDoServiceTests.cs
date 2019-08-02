984fa553aaf244a4be0af8d18c2452bdb62a30ba
szymenn <ilikedzimi@gmail.com>
2019-08-02 20:51:49 +0200

    Add ToDoServiceTests class

--- backend/test/ToDoListApi.Tests/ToDoServiceTests.cs
+++ backend/test/ToDoListApi.Tests/ToDoServiceTests.cs
@@ -1,6 +1,86 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Moq;
+using ToDoListApi.Entities;
+using ToDoListApi.Models;
+using ToDoListApi.Repositories;
+using ToDoListApi.Services;
+using Xunit;
+
 namespace ToDoListApi.Tests
 {
-    public class ToDoServiceTests : ToDoServiceTestBase
+    public class ToDoServiceTests 
     {
+        [Fact]
+        public void GetToDos_ByDefault_ReturnsToDoViewModelList()
+        {
+            var toDoRepositoryStub = new Mock<IToDoRepository>();
+            var mapperStub = new Mock<IMapper>();
+            toDoRepositoryStub.Setup(e => e.GetToDos(It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+            mapperStub.Setup(e => e.Map<ICollection<ToDoViewModel>>(It.IsAny<ICollection<ToDo>>()))
+                .Returns(new List<ToDoViewModel>());
+
+            var service = new ToDoService
+                (toDoRepositoryStub.Object, mapperStub.Object);
+
+            var result = service.GetToDos(It.IsAny<Guid>());
+            Assert.IsType<List<ToDoViewModel>>(result);
+        }
+
+        [Fact]
+        public void AddToDo_ByDefault_ReturnsToDoViewModelList()
+        {
+            var toDoRepositoryStub = new Mock<IToDoRepository>();
+            var mapperStub = new Mock<IMapper>();
+            toDoRepositoryStub.Setup(e => e.AddToDo(It.IsAny<ToDo>(), It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+            mapperStub.Setup(e => e.Map<ICollection<ToDoViewModel>>(It.IsAny<ICollection<ToDo>>()))
+                .Returns(new List<ToDoViewModel>());
+            
+            var service = new ToDoService
+                (toDoRepositoryStub.Object, mapperStub.Object);
+
+            var result = service.AddToDo(It.IsAny<ToDoBindingModel>(), It.IsAny<Guid>());
+            Assert.IsType<List<ToDoViewModel>>(result);
+        }
+
+        [Fact]
+        public void DeleteToDo_ByDefault_ReturnsToDoViewModelList()
+        {
+            var toDoRepositoryStub = new Mock<IToDoRepository>();
+            var mapperStub = new Mock<IMapper>();
+            toDoRepositoryStub.Setup(e => e.DeleteToDo(It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .Returns(new List<ToDo>());
+            mapperStub.Setup(e => e.Map<ICollection<ToDoViewModel>>(It.IsAny<ICollection<ToDo>>()))
+                .Returns(new List<ToDoViewModel>());
+            
+            var service = new ToDoService
+                (toDoRepositoryStub.Object, mapperStub.Object);
+
+            var result = service.DeleteToDo(It.IsAny<Guid>(), It.IsAny<Guid>());
+            Assert.IsType<List<ToDoViewModel>>(result);
+        }
+
+        [Fact]
+        public void UpdateToDo_ByDefault_ReturnsToDoViewModel()
+        {
+            var toDoRepositoryStub = new Mock<IToDoRepository>();
+            var mapperStub = new Mock<IMapper>();
+            toDoRepositoryStub.Setup
+                (e => e.UpdateToDo
+                    (It.IsAny<ToDo>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .Returns(new ToDo());
+            mapperStub.Setup(e => e.Map<ToDoViewModel>(It.IsAny<ToDo>()))
+                .Returns(new ToDoViewModel());
+            
+            var service = new ToDoService
+                (toDoRepositoryStub.Object, mapperStub.Object);
+
+            var result = service.UpdateToDo
+                (It.IsAny<ToDoBindingModel>(), It.IsAny<Guid>(), It.IsAny<Guid>());
+            Assert.IsType<ToDoViewModel>(result);
+        }
     }
 }
