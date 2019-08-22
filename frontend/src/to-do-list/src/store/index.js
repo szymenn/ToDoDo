21d8901d17c10343c1e041d5096cd1e1023dfdac
szymenn <ilikedzimi@gmail.com>
2019-08-22 21:05:50 +0200

    remove redux dev tools

--- frontend/src/to-do-list/src/store/index.js
+++ frontend/src/to-do-list/src/store/index.js
@@ -3,6 +3,6 @@ import reducer from '../reducers';
 import thunk from 'redux-thunk';
 
 const middleware = applyMiddleware(thunk)
-const store = createStore(reducer, compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducer, middleware);
 
 export default store
