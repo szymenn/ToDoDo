1097cc3fb791bbff81751e40418c92d0bc050802
szymenn <ilikedzimi@gmail.com>
2019-08-22 16:28:54 +0200

    change logging settings

--- backend/src/ToDoListApi/Program.cs
+++ backend/src/ToDoListApi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ToDoListApi
 {
@@ -12,6 +13,7 @@ namespace ToDoListApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) => { logging.AddEventSourceLogger(); })
                 .UseStartup<Startup>();
     }
 }
