cf56872c228cd08e5a014342c414904ffc4561ad
szymenn <ilikedzimi@gmail.com>
2019-08-22 17:37:40 +0200

    code cleanup

---
+++ backend/src/ToDoListApi/Migrations/20190822153650_InitialAzureDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListApi.Migrations
+{
+    public partial class InitialAzureDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
